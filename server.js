8d10f281f24c79e337ec2d3a1b0c6b182f938e39
Kaan <Kaan@Kaans-Mac.local>
2018-09-09 10:30:54 -0700

    finished version

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require('express')
-<<<<<<< HEAD
+
 var bodyParser = require('body-parser');
 var path = require("path"); 
-=======
-var bodyParser = require('body-parser'); 
->>>>>>> fb947c622356d037e5d5dff6e3545215ec9b5695
 
 //get express functions 
 var app = express(); 
@@ -20,20 +17,14 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-<<<<<<< HEAD
-=======
-app.use(express.static("./app/public"));
->>>>>>> fb947c622356d037e5d5dff6e3545215ec9b5695
 
+app.use(express.static("./app/public"));
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-<<<<<<< HEAD
-//require("./routing/apiRoutes")(app);
-=======
->>>>>>> fb947c622356d037e5d5dff6e3545215ec9b5695
+
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
@@ -42,9 +33,4 @@ app.listen(PORT, function(err){
     if (err) throw err;
 
     console.log("Port open on: " + PORT);
-<<<<<<< HEAD
-})
-
-=======
-})
->>>>>>> fb947c622356d037e5d5dff6e3545215ec9b5695
+});
