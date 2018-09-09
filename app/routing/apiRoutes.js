30bacbd6e031db419ecc463d030c9f4436a9d71f
Kaan <Kaan@Kaans-Mac.local>
2018-09-09 10:18:46 -0700

    new changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,6 +3,49 @@ var friendList = require("../data/friends.js");
 
 //Whenever you'll hit "/api/friends" route without clicking Submit button, It will display data from firends.js filwe
 module.exports = function(app){
+<<<<<<< HEAD
+    app.get('/api/friends',function(req,res){
+        res.json(friendList);
+    });
+
+
+//Post method to handle "/api/friends" route by clicking Submit button
+app.post('/api/friends', function(req,res){
+    //New freind's data from Survey.html page
+    var newFriend = req.body;
+    var newFriendScore = newFriend.scores;
+    console.log("Friends score array "+newFriendScore);
+    var scoresArray = [];
+    var friendCount = 0;
+    var bestMatch = 0;
+
+
+    //Run through the scores to compare friends from Friends.js file
+    for(var i=0;i<friendList.length;i++){
+        var scoresDiff = 0;
+
+        for(var j=0;j<newFriendScore.length;j++){
+            scoresDiff = scoresDiff + (Math.abs(parseInt(friendList[i].scores[j])- parseInt(newFriendScore[j])));
+        }
+
+        scoresArray.push(scoresDiff);
+        console.log("Scores array: "+scoresArray);
+    }
+
+    for(var i=0; i <scoresArray.length;i++){
+        if(scoresArray[i] <= scoresArray[bestMatch])
+        {
+            bestMatch = i;
+        }
+    }
+
+    var bff = friendList[bestMatch];
+    res.json(bff);
+
+    friendList.push(newFriend);
+});
+}
+=======
     
     app.get('/api/friends',function(req,res){
         res.json(friendList);
@@ -52,3 +95,4 @@ module.exports = function(app){
 
 }; // end of modules
 
+>>>>>>> fb947c622356d037e5d5dff6e3545215ec9b5695
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 var express = require('express')
+<<<<<<< HEAD
+var bodyParser = require('body-parser');
+var path = require("path"); 
+=======
 var bodyParser = require('body-parser'); 
+>>>>>>> fb947c622356d037e5d5dff6e3545215ec9b5695
 
 //get express functions 
 var app = express(); 
@@ -15,13 +20,20 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+<<<<<<< HEAD
+=======
 app.use(express.static("./app/public"));
+>>>>>>> fb947c622356d037e5d5dff6e3545215ec9b5695
 
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
+<<<<<<< HEAD
+//require("./routing/apiRoutes")(app);
+=======
+>>>>>>> fb947c622356d037e5d5dff6e3545215ec9b5695
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
@@ -30,4 +42,9 @@ app.listen(PORT, function(err){
     if (err) throw err;
 
     console.log("Port open on: " + PORT);
+<<<<<<< HEAD
 })
+
+=======
+})
+>>>>>>> fb947c622356d037e5d5dff6e3545215ec9b5695
